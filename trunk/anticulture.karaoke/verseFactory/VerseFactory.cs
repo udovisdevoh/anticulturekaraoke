6259f21b3ca4520cd0dc3b2377b2dbf2a4d41091
glacasse <glacasse@273d3e06-d9ed-11de-8314-133c20781edd>
2009-11-25 18:22:35 +0000

    Added stubs for Verse andVerseFactory

---
+++ trunk/anticulture.karaoke/verseFactory/VerseFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace anticulture.karaoke.verseFactory
+{
+    /// <summary>
+    /// Produces verses
+    /// </summary>
+    public static class VerseFactory
+    {
+    }
+}
