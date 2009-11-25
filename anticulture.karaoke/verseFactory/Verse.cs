e9e7ccaae78a63c8b31662de5d2698e18fe9a7c2
glacasse <glacasse@273d3e06-d9ed-11de-8314-133c20781edd>
2009-11-25 18:22:35 +0000

    Added stubs for Verse andVerseFactory

---
+++ anticulture.karaoke/verseFactory/Verse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace anticulture.karaoke.verseFactory
+{
+    /// <summary>
+    /// Represents a song's verse
+    /// </summary>
+    public class Verse
+    {
+    }
+}
