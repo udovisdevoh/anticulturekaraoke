502165d0ec0ecaa1d35fd8cbb9bf401af4c5db24
glacasse <glacasse@273d3e06-d9ed-11de-8314-133c20781edd>
2009-11-25 18:11:45 +0000

    Created project

---
+++ anticulture.karaoke/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace anticulture.karaoke
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
