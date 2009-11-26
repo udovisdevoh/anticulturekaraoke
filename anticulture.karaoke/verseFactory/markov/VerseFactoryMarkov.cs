8d91d3f2967cadacd0103139fc2849bca137ce03
glacasse <glacasse@273d3e06-d9ed-11de-8314-133c20781edd>
2009-11-26 16:09:09 +0000

    Markov algorithm: Consider verse length

--- anticulture.karaoke/verseFactory/markov/VerseFactoryMarkov.cs
+++ anticulture.karaoke/verseFactory/markov/VerseFactoryMarkov.cs
@@ -15,6 +15,11 @@ namespace anticulture.karaoke.verseFactory
         /// Sampling size
         /// </summary>
         private const int SamplingSize = 200;
+
+        /// <summary>
+        /// Minimum count verse ending with stop marker
+        /// </summary>
+        private const int MinimumCountEndingWithStop = 10;
         #endregion
 
         #region Public Methods
@@ -25,23 +30,62 @@ namespace anticulture.karaoke.verseFactory
         /// <returns>verse generated using markov chains</returns>
         public static Verse Build(Verse previousVerse)
         {
+            IList<Verse> verseList = GetMarkovVerseList(previousVerse, SamplingSize);
+            verseList = TryKeepOnlyEndingWithStop(verseList);
+            return Evaluator.PickBestLength(verseList, VerseFactory.DesiredLength);
+        }
+        #endregion
+
+        #region Private Methods
+        /// <summary>
+        /// Get a list of markovly generated verse
+        /// </summary>
+        /// <param name="previousVerse">previous verse</param>
+        /// <param name="samplingSize">sampling size</param>
+        /// <returns>list of markovly generated verse</returns>
+        private static IList<Verse> GetMarkovVerseList(Verse previousVerse, int samplingSize)
+        {
             IEnumerable<Verse> verseListStraight = VerseFactory.LyricSource.GetRandomContiguousSourceLineList(VerseFactory.Random, SamplingSize);
             IEnumerable<Verse> verseListReversed = VerseFactory.LyricSourceReversed.GetRandomContiguousSourceLineList(VerseFactory.Random, SamplingSize);
 
-            WordMatrix wordMatrix = new WordMatrix(verseListStraight, verseListReversed);
-            wordMatrix.resetCursor();
-            string verseContent = string.Empty;
-            string currentWord = null;
+            List<Verse> verseList = new List<Verse>();
 
-            do
+            for (int i = 0; i < samplingSize; i++)
             {
-                currentWord = wordMatrix.GenerateNextWord(VerseFactory.Random);
-                verseContent += " " + currentWord;
-            } while (verseContent.Length < VerseFactory.DesiredLength - 5 && currentWord != null) ;
+                WordMatrix wordMatrix = new WordMatrix(verseListStraight, verseListReversed);
+                wordMatrix.resetCursor();
+                string verseContent = string.Empty;
+                string currentWord = null;
 
-            verseContent = verseContent.HardTrim();
+                do
+                {
+                    currentWord = wordMatrix.GenerateNextWord(VerseFactory.Random);
+                    verseContent += " " + currentWord;
+                } while (verseContent.Length < VerseFactory.DesiredLength - 5 && currentWord != null);
 
-            return new Verse(verseContent);
+                verseContent = verseContent.HardTrim();
+                verseList.Add(new Verse(verseContent));
+            }
+
+            return verseList;
+        }
+
+        /// <summary>
+        /// Try keep only verses that end with real end
+        /// </summary>
+        /// <param name="verseList">verse list</param>
+        /// <returns>verses that end with real end or just random verse if not possible</returns>
+        private static IList<Verse> TryKeepOnlyEndingWithStop(IList<Verse> verseList)
+        {
+            List<Verse> listEndingWithStop = new List<Verse>();
+            foreach (Verse currentVerse in verseList)
+                if (currentVerse.ToString().EndsWith("[stop]"))
+                    listEndingWithStop.Add(currentVerse);
+            
+            if (listEndingWithStop.Count >= MinimumCountEndingWithStop)
+                return listEndingWithStop;
+            else
+                return verseList;
         }
         #endregion
     }
