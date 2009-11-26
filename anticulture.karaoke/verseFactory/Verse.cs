d21b9f805e82f3e9cf4ae8f3319902343e0d2fd2
glacasse <glacasse@273d3e06-d9ed-11de-8314-133c20781edd>
2009-11-26 00:40:05 +0000

    Theme Matcher

--- anticulture.karaoke/verseFactory/Verse.cs
+++ anticulture.karaoke/verseFactory/Verse.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using anticulture.karaoke.themes;
 
 namespace anticulture.karaoke.verseFactory
 {
@@ -15,6 +16,11 @@ namespace anticulture.karaoke.verseFactory
         /// Text value
         /// </summary>
         private string textValue;
+
+        /// <summary>
+        /// Score for each theme (don't use directly: lazy initialization)
+        /// </summary>
+        private Dictionary<string, float> _themeScore;
         #endregion
 
         #region Constructors
@@ -36,6 +42,37 @@ namespace anticulture.karaoke.verseFactory
         public override string ToString()
         {
             return textValue;
+        }
+
+        /// <summary>
+        /// Get matching score for selected theme
+        /// </summary>
+        /// <param name="themeName">theme to match</param>
+        /// <returns> score for theme</returns>
+        public float GetScore(string themeName)
+        {
+            float score;
+            if (!ThemeScore.TryGetValue(themeName, out score))
+            {
+                score = ThemeMatcher.Match(textValue, themeName);
+                ThemeScore.Add(themeName, score);
+            }
+            return score;
+        }
+        #endregion
+
+        #region Private Properties
+        /// <summary>
+        /// Score for teach theme
+        /// </summary>
+        public Dictionary<string, float> ThemeScore
+        {
+            get
+            {
+                if (_themeScore == null)
+                    _themeScore = new Dictionary<string, float>();
+                return _themeScore;
+            }
         }
         #endregion
     }
