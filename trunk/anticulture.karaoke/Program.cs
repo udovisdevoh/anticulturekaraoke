ea53b4a34fd5eae7cfb02c32fc12b77f9d3144bb
glacasse <glacasse@273d3e06-d9ed-11de-8314-133c20781edd>
2009-12-18 01:29:51 +0000

    Added unit tests

--- trunk/anticulture.karaoke/Program.cs
+++ trunk/anticulture.karaoke/Program.cs
@@ -11,69 +11,7 @@ namespace anticulture.karaoke
     {
         static void Main(string[] args)
         {
-            VerseFactory verseFactory = new VerseFactory();
-
-            //verseFactory.AddTheme(ThemeLoader.Load("urban"));
-            //verseFactory.AddTheme(ThemeLoader.Load("nature"));
-            //verseFactory.AddTheme(ThemeLoader.Load("food"));
-            verseFactory.AddTheme(ThemeLoader.Load("activism"));
-            verseFactory.AddTheme(ThemeLoader.Load("fantastic"));
-            verseFactory.AddTheme(ThemeLoader.Load("philosophy"));
-            verseFactory.AddTheme(ThemeLoader.Load("geek"));
-            verseFactory.AddTheme(ThemeLoader.Load("metaphysics"));
-            //verseFactory.AddTheme(ThemeLoader.Load("food"));
-            //verseFactory.AddTheme(ThemeLoader.Load("religion"));
-            //verseFactory.AddTheme(ThemeLoader.Load("money"));
-            //verseFactory.AddTheme(ThemeLoader.Load("gore"));
-            //verseFactory.AddTheme(ThemeLoader.Load("gangsta"));
-            //verseFactory.AddTheme(ThemeLoader.Load("shame"));
-
-            Verse verse;
-
-            verseFactory.ClearCreationMemory();
-
-            //verseFactory.Algorithm = VerseConstructionSettings.AlgorithmAnalogy;
-            verseFactory.Algorithm = VerseConstructionSettings.AlgorithmInterleavedAnalogy;
-            //verseFactory.Algorithm = VerseConstructionSettings.AlgorithmStraight;
-
-            verseFactory.DesiredLength = 32;
-
-            for (int i = 0; i < 16; i++)
-            {
-                verse = verseFactory.Build();
-                Console.WriteLine(verse.ToString());
-                if ((i + 1) % 4 == 0)
-                    Console.WriteLine("");
-            }
-
-            /*verseFactory.Algorithm = VerseConstructionSettings.AlgorithmAnalogy;
-
-            verse = verseFactory.Build();
-            Console.WriteLine(verse.ToString());
-            verse = verseFactory.Build();
-            Console.WriteLine(verse.ToString());
-            verse = verseFactory.Build();
-            Console.WriteLine(verse.ToString());*/
-
-            //verseFactory.ClearCreationMemory();
-
-            //verse = verseFactory.Build();
-            //Console.WriteLine(verse.ToString());
-
-            /*verseFactory.Algorithm = VerseConstructionSettings.AlgorithmMarkov;
-            verse = verseFactory.Build(verse);
-            Console.WriteLine(verse.ToString());
-
-            verseFactory.Algorithm = VerseConstructionSettings.AlgorithmCryptic;
-            verse = verseFactory.Build(verse);
-            Console.WriteLine(verse.ToString());
-
-            verseFactory.Algorithm = VerseConstructionSettings.AlgorithmMarkov;
-            verse = verseFactory.Build(verse);
-            Console.WriteLine(verse.ToString());*/
-
-
-            Console.ReadLine();
+            UnitTests.TestAll();            
         }
     }
 }
