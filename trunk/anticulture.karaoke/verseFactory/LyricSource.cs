8e2c3ac6e6195f7d752a1368ad96d15ade3bcd97
glacasse <glacasse@273d3e06-d9ed-11de-8314-133c20781edd>
2010-04-12 19:19:59 +0000

    Updated artificial art library

--- trunk/anticulture.karaoke/verseFactory/LyricSource.cs
+++ trunk/anticulture.karaoke/verseFactory/LyricSource.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
-using Linguistics;
+using ArtificialArt.Linguistics;
 
 namespace anticulture.karaoke.verseFactory
 {
