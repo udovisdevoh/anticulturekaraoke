8fece702e23a31f6ec8fe9df3f9de45c7bf2fef1
glacasse <glacasse@273d3e06-d9ed-11de-8314-133c20781edd>
2010-04-12 19:19:59 +0000

    Updated artificial art library

--- anticulture.karaoke/verseFactory/parts/analogy/AnalogyManager.cs
+++ anticulture.karaoke/verseFactory/parts/analogy/AnalogyManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using anticulture.karaoke.themes;
-using Markov;
+using ArtificialArt.Markov;
 
 namespace anticulture.karaoke.verseFactory
 {
